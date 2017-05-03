8cfc0159ae938252fc8f203af2d6e4b6651c11e3
bunnie <bunnie@kosagi.com>
2017-05-03 08:23:13 +0000

    fix shipmode UI

--- src/app-shipmode.c
+++ src/app-shipmode.c
@@ -2,7 +2,7 @@
 #include "charger.h"
 #include "userconfig.h"
 
-static uint8_t modeintent = 0;
+static uint8_t pluggedin = 0;
 
 static void redraw_ui(void) {
   coord_t width;
@@ -20,31 +20,21 @@ static void redraw_ui(void) {
   // draw title box
   gdispClear(Black);
 
-  gdispDrawStringBox(0, height * 2, width, height,
-                     "Select option...", font, White, justifyCenter);
-
-  if( modeintent == 0 ) {
-    text_color = Black;
-    bg_color = White;
+  if( pluggedin ) {
+    gdispDrawStringBox(0, height * 2, width, height,
+		       "Can't power down", font, White, justifyCenter);
+    gdispDrawStringBox(0, height * 3, width, height,
+		       "while charging, unplug", font, White, justifyCenter);
+    gdispDrawStringBox(0, height * 4, width, height,
+		       "to continue...", font, White, justifyCenter);
   } else {
-    text_color = White;
-    bg_color = Black;
+    gdispDrawStringBox(0, height * 2, width, height,
+		       "Press select to", font, White, justifyCenter);
+    gdispDrawStringBox(0, height * 3, width, height,
+		       "power off, or hold", font, White, justifyCenter);
+    gdispDrawStringBox(0, height * 4, width, height,
+		       "home to abort...", font, White, justifyCenter);
   }
-    
-  gdispFillArea(0, height*4, width, height, bg_color);
-  gdispDrawStringBox(0, height * 4, width, height,
-		     "Standby (recommended)", font, text_color, justifyCenter);
-
-  if( modeintent != 0 ) {
-    text_color = Black;
-    bg_color = White;
-  } else {
-    text_color = White;
-    bg_color = Black;
-  }
-  gdispFillArea(0, height*5, width, height, bg_color);
-  gdispDrawStringBox(0, height * 5, width, height,
-                     "Disconnect battery", font, text_color, justifyCenter);
   
   gdispFlush();
   gdispCloseFont(font);
@@ -56,7 +46,9 @@ static void shipmode_start(OrchardAppContext *context) {
   (void)context;
 
   configFlush();  // flush all volatile state before shutting down
+  
   redraw_ui();
+  orchardAppTimer(context, 125 * 1000 * 1000, true);
 }
 
 void shipmode_event(OrchardAppContext *context, const OrchardAppEvent *event) {
@@ -64,21 +56,12 @@ void shipmode_event(OrchardAppContext *context, const OrchardAppEvent *event) {
 
   if (event->type == keyEvent) {
     if ((event->key.flags == keyDown) && (event->key.code == keySelect)) {
-      if( modeintent == 0 ) {
-	orchardTestPrompt("Standing by...", "hit reset to wake", 0);
-	//	halt();  /////// TODO
-      } else {
-	chThdYield();
-	chThdSleepMilliseconds(300); // wait for previous touch state to drain
-	if(orchardTestPrompt("Are you sure?", "Press key to confirm", 5) == orchardResultPass )
-	  chargerShipMode();
-	else
-	  redraw_ui();
-      }
-    } else if( event->key.flags == keyDown ) {
-      modeintent = !modeintent;
-      redraw_ui();
+      if( !pluggedin )
+	chargerShipMode();
     }
+  } else if (event->type == timerEvent) {
+    pluggedin = isCharging();
+    redraw_ui();
   }
 }
 
