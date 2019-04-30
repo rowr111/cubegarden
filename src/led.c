6ab041d894edc80915afa081aff1e6a5e2da5c13
Jeanie <jeanie.conner@gmail.com>
2019-05-01 00:40:06 +0900

    added basic effect using accel data

--- src/led.c
+++ src/led.c
@@ -6,6 +6,7 @@
 
 #include "mic.h"
 #include "analog.h"
+#include "accel.h"
 
 #include "chprintf.h"
 #include "stdlib.h"
@@ -616,6 +617,56 @@ static void dbColorChangeAndIntensityEffect(struct effects_config *config) {
   }
 }
 orchard_effects("DBcolor", dbColorChangeAndIntensityEffect);
+
+static void accelEffect(struct effects_config *config) {
+  //todo: it does a weird color switch at 0 degrees for some reason, fix this
+
+  uint8_t *fb = config->hwconfig->fb;
+  int count = config->count;
+  //int loop = config->loop; //currently unused
+  int i;
+
+  //let's get the xyz coordinates
+  struct accel_data data;
+  int x, y, z;
+  accelPoll(&data);
+  if( data.x >= 2048 ) {
+    x = (int)data.x - 4095;
+    } else x = data.x;
+  if( data.y >= 2048 ) {
+    y = (int)data.y - 4095;
+    } else y = data.y;
+  if( data.z >= 2048 ) {
+      z = (int)data.z - 4095;
+    } else z = data.z;
+
+  //let's make an angle from the xy coordinates
+  float angle = atan2(-y, -x) * (180/3.14159) - 90;
+  angle = angle < 0 ? 360 + angle : angle;  // Ensure positive angle
+  //convert it into the hue 
+	int angleHue = (int)((angle/360)*255);
+  //do some slight smoothing:
+  static int avg1;
+  static int avg2;
+  static int avg3;
+  static int sum;
+  sum = sum - avg1;
+  avg1 = avg2;
+  avg2 = avg3;
+  avg3 = angleHue;
+  sum = sum + avg3;
+  int avgAngle = sum/3;
+
+	//then convert to 0-255 for hsv color to make it purdy
+	HsvColor angleHSV = {avgAngle, 255, 255};
+
+	//convert back to rgb and set the LED color
+	RgbColor c = HsvToRgb(angleHSV);
+	for (i = 0; i < count; i++) {
+		ledSetRGB(fb, i, c.r, c.g, c.b, shift);
+	}
+}
+orchard_effects("accel", accelEffect);
 
 /*
 static void basicDrop(struct effects_config *config){
