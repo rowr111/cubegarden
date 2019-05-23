24ca6ed6fd5b869691cd8e81bdaaf2e41722b870
bunnie <bunnie@kosagi.com>
2019-05-23 12:41:19 +0100

    add master badge bypass so strobe isn't annoying

--- src/fx-strobe.c
+++ src/fx-strobe.c
@@ -8,6 +8,8 @@
 #include <string.h>
 #include <math.h>
 
+#ifndef MASTER_BADGE
+
 static void strobePatternFB(struct effects_config *config) {
   uint8_t *fb = config->hwconfig->fb;
   int count = config->count;
@@ -42,3 +44,40 @@ static void strobePatternFB(struct effects_config *config) {
 }
 orchard_effects("strobe", strobePatternFB);
 
+#else
+
+static void strobePatternFB(struct effects_config *config) {
+  uint8_t *fb = config->hwconfig->fb;
+  int count = config->count;
+  
+  uint16_t i;
+  uint8_t oldshift = shift;
+  static uint32_t  nexttime = 0;
+  static uint8_t   strobemode = 1;
+  
+  shift = 0;
+
+  if( strobemode && (chVTGetSystemTime() > nexttime) ) {
+    for( i = 0; i < count; i++ ) {
+      if( (rand() % (unsigned int) count) < ((unsigned int) count / 3) )
+	ledSetRGB(fb, i, 8, 8, 8, shift);
+      else
+	ledSetRGB(fb, i, 0, 0, 0, shift);
+    }
+
+    nexttime = chVTGetSystemTime() + 30 + (rand() % 25);
+    strobemode = 0;
+  }
+
+  else if( !strobemode && (chVTGetSystemTime() > nexttime) ) {
+    ledSetAllRGB(fb, count, 0, 0, 0, shift);
+    
+    nexttime = chVTGetSystemTime() + 30 + (rand() % 25);
+    strobemode = 1;
+  }
+
+  shift = oldshift;
+}
+orchard_effects("strobe", strobePatternFB);
+
+#endif
