fcaeeaa43463dd082f0222cd40731994c79ed3fd
bunnie <bunnie@kosagi.com>
2019-05-25 14:47:07 +0100

    fix up function scoping in cmd-forward subsystem

--- src/cmd-forward.h
+++ src/cmd-forward.h
@@ -5,10 +5,7 @@
 #include "hal.h"
 #include "shell.h"
 
-
 void handleRadioForward(uint8_t prot, uint8_t src, uint8_t dst, uint8_t length, const void *data);
-static char *parse_arguments(char *str, char **saveptr);
-static bool cmdexec(const ShellCommand *scp, BaseSequentialStream *chp,
-                      char *name, int argc, char *argv[]);
+void cmd_forward(BaseSequentialStream *chp, int argc, char *argv[]);
 
 #endif // __CMD_FORWARD__
