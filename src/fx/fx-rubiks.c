39716221e397c1c66eeb30882c415db8c60eab3f
drakeley <sara.drakeley@mobilecoin.com>
2019-08-04 13:38:40 -0700

    Add colors for top

--- src/fx/fx-rubiks.c
+++ src/fx/fx-rubiks.c
@@ -25,8 +25,8 @@ static const int NUMSIDES = 6;
 /*effect description - this is a game:
 1. each cube starts as a random rubiks color (red, green, blue, yellow, white, magenta)
 2. each cube will statically remain that color
-  d. tilting all the way to 90 deg will cause the cube to be fixed on a particular color, depending on the side.
-3. once all cubes are the same color, master badge will trigger "you won!" effect
+  d. tilting a cube all the way to 90 degrees (or a little less) will cause the cube to change color
+3. once the minimum number of cubes are the same color, master badge will trigger "you won!" effect
 */
 static void rubiks(struct effects_config *config) {
   uint8_t *fb = config->hwconfig->fb;
@@ -42,7 +42,6 @@ static void rubiks(struct effects_config *config) {
     colorindex++; //need to be on a scale of 1-numOfBaseHsvColors
   }
 
-  //if cube is tilted all the way on its side, it will be fixed at a base color and not pulse
   if(z_inclination > 75 && z_inclination < 105){
     RgbColor c;
     if (current_side == 0) {
@@ -55,9 +54,17 @@ static void rubiks(struct effects_config *config) {
       c = YELLOW;
     }
     ledSetAllRgbColor(fb, count, c, shift); // FIXME: set *all*?
+  } else {
+      if (z_inclination <= 75) {
+          c = MAGENTA;
+      } else {
+          c = WHITE;
+      }
   }
 }
 orchard_effects("rubiks", rubiks, 0);
+
+// FIXME: How to tell that all cubes are the same color?
 
 #else
 static void rubiks(struct effects_config *config) {
