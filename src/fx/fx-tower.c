a6fd324507f78fd9472b1d365a526bbba3c33c42
Tobias Wennergren <wennergr#gmail.com>
2019-08-04 18:35:59 -0700

    Direct access of pressure

--- src/fx/fx-tower.c
+++ src/fx/fx-tower.c
@@ -16,23 +16,25 @@
 #include "gfx.h"
 #include "trigger.h"
 
-float baseline = null;
+#define FUNDEFINE -0.0
+
+float baseline = FUNDEFINE;
 
 static void tower(struct effects_config *config) {
-    uint8_t *fb = config->hwconfig->fb;
     int loop = config->loop;
 
     if (!baro_avg_valid) {
         chprintf(stream, "Waiting for barometer to be available\n\r");
         return;
     } else {
+        
+        if (baseline == FUNDEFINE) {
+            baseline = baro_pressure;
+        }
 
-    if (baseline == null) {
-        baseline = baro_avg;
-    }
-
-    if (loop % 30 == 0) {
-        chprintf(stream, "%f\n\r", baro_avg - baseline);
+        if (loop % 30 == 0) {
+            chprintf(stream, "%f\n\r", baro_pressure - baseline);
+        }
     }
 }
 
