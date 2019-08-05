5e5095a5b7519929ffa94e0a9d8b6c62da4414f6
Howard Lo <howard.lo@bitmex.com>
2019-08-04 19:56:06 -0700

    Adding wait for baseline value

--- src/fx/fx-tower.c
+++ src/fx/fx-tower.c
@@ -19,7 +19,8 @@
 #define FUNDEFINE -0.0
 
 float baseline = FUNDEFINE;
-int whenToStart = 0;
+static int whenToStart = 0;
+static int brightness = 5;
 
 static void tower(struct effects_config *config) {
     uint8_t *fb = config->hwconfig->fb;
@@ -28,8 +29,9 @@ static void tower(struct effects_config *config) {
 
     if (!baro_avg_valid) {
         chprintf(stream, "Waiting for barometer to be available\n\r");
-        whenToStart = loop + 35;
         return;
+    } else if (loop < whenToStart) {
+        chprintf(stream, "Getting baseline...\n\r");
     } else if (loop > whenToStart) {
         
         if (baseline == FUNDEFINE) {
@@ -44,11 +46,12 @@ static void tower(struct effects_config *config) {
         }
 
         if (loop % 10 == 0) {
+
             int delta = (int)abs(baro_avg - baseline);
 
             RgbColor color = {delta, delta, delta};
 
-            ledSetAllRGB(fb, count, color.r, color.g, color.b, shift);
+            ledSetAllRGB(fb, count, color.r, 0, 0, shift);
 
             chprintf(stream, "%f\n\r", baro_pressure - baseline);
         }
