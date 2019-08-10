765672e155dccc544ba5fce6091766547b02d395
Johan Boberg <boberj@users.noreply.github.com>
2019-08-10 00:19:41 -0700

    Pearl fx

---
+++ src/fx/fx-pearl.c
@@ -0,0 +1,36 @@
+#include "ch.h"
+#include "hal.h"
+#include "led.h"
+#include "orchard-effects.h"
+#include "chprintf.h"
+#include "stdlib.h"
+#include "radio.h"
+
+#include <string.h>
+#include <math.h>
+
+static void fx(struct effects_config *config){
+  static bool started = false;
+  static RgbColor source = {0, 0, 0};
+  static RgbColor destination;
+  static int steps = 10;
+
+  uint8_t *fb = config->hwconfig->fb;
+  int count = config->count;
+  int loop = config->loop;
+  int step = loop % steps;
+
+  if (!started || step == 0) {
+    started = true;
+    HsvColor hsv = {(uint8_t) (rand() % 256), 255, 255};
+    destination = HsvToRgb(hsv);
+  }
+
+  source.r = source.r + (destination.r - source.r) / (steps - step);
+  source.g = source.g + (destination.g - source.g) / (steps - step);
+  source.b = source.b + (destination.b - source.b) / (steps - step);
+
+  ledSetAllRgbColor(fb, count, source, shift);
+}
+
+orchard_effects("pearl", fx, 0);
