d665b462172a29c95e5904a3b10a19717190fe69
Jeanie <rowr111@users.noreply.github.com>
2022-07-11 23:08:52 +0900

    fix

--- src/fx/generic-color.c
+++ src/fx/generic-color.c
@@ -22,7 +22,7 @@ upon single tap, switch who you are
 */
 
 
-static void genericColor(struct effects_config config, int colors[], int numOfColors) {
+static void genericColor(struct effects_config config, uint16_t colors[], uint_8 numOfColors) {
     uint8_t *fb = config->hwconfig->fb;
     int count = config->count;
     int loop = config->loop; 
